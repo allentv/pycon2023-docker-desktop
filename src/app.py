aeb8d5561a2377819ec9c1633436d3c52a402b6c
Allen Thomas Varghese <allentv4u@gmail.com>
2023-09-04 20:31:26 +0100

    Add table

--- src/app.py
+++ src/app.py
@@ -1,9 +1,21 @@
 import PySimpleGUI as sg
 
+sg.ChangeLookAndFeel("LightGrey")
+
 layout = [
     [sg.Text("Persistent window")],
     [sg.Input(key="-IN-")],
     [sg.Button("Read"), sg.Exit()],
+    [
+        sg.Table(
+            [[1, 2, 3], [11, 22, 33], [111, 222, 333]],
+            headings=["c1", "c2", "c3"],
+            auto_size_columns=True,
+            enable_events=True,
+            enable_click_events=False,
+            key="-TABLE-",
+        )
+    ],
 ]
 
 window = sg.Window("Window that stays open", layout)
